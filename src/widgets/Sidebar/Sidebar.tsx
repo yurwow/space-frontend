c7093c3a1324ecd3f858f9b8bdaddd1708280f19
yurwow <yura_kuzin_03@mail.ru>
2024-03-25 00:39:33 +0500

    linter config change

--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -4,25 +4,32 @@ import {
 import classNames from './Sidebar.module.css';
 import { NavigationMenu } from '@/widgets/Menu';
 
-export const Sidebar = () => {
-
-  return (
-    <Paper className={classNames.sidebar} sx={{ py: 6, px: 4 }}>
-      <Stack justifyContent="space-between" height="100%">
-        <Stack spacing={4}>
-          <Typography variant="h1">Space</Typography>
-          <NavigationMenu />
-        </Stack>
-        <Stack
-          className={classNames.user}
-          direction="row"
-          spacing={2}
-          alignItems="center"
-        >
-          <Avatar sx={{ width: 40, height: 40 }} />
-          <Typography variant="h4">Пользователь</Typography>
-        </Stack>
+export const Sidebar = () => (
+  <Paper
+    className={classNames.sidebar}
+    sx={{
+      py: 6,
+      px: 4,
+    }}
+  >
+    <Stack justifyContent="space-between" height="100%">
+      <Stack spacing={4}>
+        <Typography variant="h1">Space</Typography>
+        <NavigationMenu />
       </Stack>
-    </Paper>
-  );
-};
+      <Stack
+        className={classNames.user}
+        direction="row"
+        spacing={2}
+        alignItems="center"
+      >
+        <Avatar sx={{
+          width: 40,
+          height: 40,
+        }}
+        />
+        <Typography variant="h4">Пользователь</Typography>
+      </Stack>
+    </Stack>
+  </Paper>
+);
